44cd7ff93227bd16799b02c37ee0d37e06cbf6c0
kubo578 <chass.matharu@gmail.com>
2020-05-01 15:50:51 +0100

    redux middleware

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './containers/App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
+import { createLogger } from 'redux-logger';
 import { searchRobots } from './reducers'
 import 'tachyons';
 
-
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers.js
+++ src/reducers.js
@@ -5,7 +5,6 @@ const intialState = {
 }
 
 export const searchRobots = (state=intialState, action={}) => {
-	console.log (action.type);
 	switch(action.type) {
 		case CHANGE_SEARCH_FIELD:
 		  return Object.assign({}, state, {searchField: action.payload});
