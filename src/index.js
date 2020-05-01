2c6e07d2c5b1426c8a37984941e0032294ac5dfd
kubo578 <chass.matharu@gmail.com>
2020-05-01 08:46:08 +0100

    Redux Store and Provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import 'tachyons';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers'
+import 'tachyons';
+
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-     <App />;
+     <Provider store = {store}>
+	     <App />;
+     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
